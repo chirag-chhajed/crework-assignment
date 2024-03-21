18b9246528d6f0b64a61d56b47c0e30727389def
Chirag Chhajed <chhajedchirag18@gmail.com>
2024-03-22 00:03:57 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,20 @@ export const metadata: Metadata = {
   title: "Crework",
   description:
     "Get your next product job with our community first immersive cohort",
+  openGraph: {
+    title: "Crework",
+    description:
+      "Get your next product job with our community first immersive cohort",
+    siteName: "Crework",
+    images: [
+      {
+        url: "https://res.cloudinary.com/crework-cloud/image/upload//c_thumb,w_1200,h_630/v1709912646/Preview_Image_PM_Cohort_a11edk.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
